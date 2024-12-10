467c06bebf0ea3d778f2275463ff81b5fbdfcc9f
fynn <fynn-git.wi04u@slmail.me>
2024-12-10 08:08:39 +0100

    add 06_1

---
+++ script/aoc_06_1.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from aoc import (
+    PuzzleName,
+    Dir,
+)
+
+
+def get_input_values(file_path: Path):
+    values = []
+
+    with open(file_path, 'r') as file:
+        for line in file.readlines():
+            if not line:
+                continue
+
+    return values
+
+
+def main(file_path: Path | None = None):
+    script_path = Path(__file__)
+    puzzle_name = PuzzleName.parse(script_path.stem)
+    file_path = file_path or Dir.build_file_path(
+        Dir.INPUT,
+        puzzle_name=PuzzleName(
+            day=puzzle_name.day,
+            part=puzzle_name.part,
+        )
+    )
+    result = "I am result"
+    return result
+
+
+if __name__ == "__main__":
+    RESULT = main()
+    print(f"Result: {RESULT}")
