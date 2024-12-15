ec47002f9b1c6574c59cfeedcf0fe1daeaa3f6cf
fynn <fynn-git.wi04u@slmail.me>
2024-12-15 18:03:45 +0100

    add 08_1

---
+++ test/test_day08_1.py
@@ -0,0 +1,33 @@
+import sys
+from importlib import util
+
+import pytest
+import aoc
+
+
+@pytest.fixture
+def something():
+    return
+
+
+def test_main():
+    puzzle_name = aoc.PuzzleName(
+        day=8,
+        part=1,
+    )
+    puzzle_name_text = puzzle_name.build()
+    input_file = aoc.Dir.build_file_path(
+        aoc.directory.Dir.INPUT,
+        puzzle_name,
+    )
+    python_file = aoc.directory.Dir.build_file_path(
+        aoc.Dir.SCRIPT,
+        puzzle_name,
+    )
+    spec = util.spec_from_file_location(puzzle_name_text, python_file)
+    module = util.module_from_spec(spec)
+    sys.modules[puzzle_name_text] = module  # Optional: add the module to sys.modules
+    spec.loader.exec_module(module)
+
+    result = module.main(input_file)
+    assert result == 2500
