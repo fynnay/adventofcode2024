18aaef4f65b758244d3a3fd937c1c4fe9377a293
fynn <fynn-git.wi04u@slmail.me>
2024-12-15 18:43:34 +0100

    add riddle

--- script/aoc_08_1.py
+++ script/aoc_08_1.py
@@ -1,3 +1,14 @@
+"""
+The signal only applies its nefarious effect at specific antinodes based on the resonant frequencies of the antennas.
+In particular, an antinode occurs at any point that is perfectly in line with two antennas of the same frequency -
+but only when one of the antennas is twice as far away as the other. This means that for any pair of antennas with
+the same frequency, there are two antinodes, one on either side of them.
+
+Antennas with different frequencies don't create antinodes; A and a count as different frequencies.
+However, antinodes can occur at locations that contain antennas.
+
+
+"""
 from pathlib import Path
 
 from aoc import (
@@ -17,6 +28,11 @@ def get_input_values(file_path: Path):
     return values
 
 
+def process(input_values):
+    print(input_values)
+    return input_values
+
+
 def main(file_path: Path | None = None):
     script_path = Path(__file__)
     puzzle_name = PuzzleName.parse(script_path.stem)
@@ -27,7 +43,8 @@ def main(file_path: Path | None = None):
             part=puzzle_name.part,
         )
     )
-    result = "I am result"
+    input_values = get_input_values(file_path)
+    result = process(input_values)
     return result
 
 
