92394574cd09e4d1cff68a86f4bb329d063ec02c
fynn <fynn-git.wi04u@slmail.me>
2024-12-26 18:41:04 +0100

    update test values for new way of unpacking and calculating the checksum

--- test/test_09_01.py
+++ test/test_09_01.py
@@ -10,9 +10,9 @@ from aoc_09_1 import unpack, reorder, calculate_checksum
 
 class ValueType(Enum):
     SHORT = "short"
+    MEDIUM = "medium"
     LONG = "long"
     UNEVEN = "uneven"
-
 
 class Value:
     def __init__(
@@ -24,22 +24,26 @@ class Value:
         self.unpacked: list[str]
         self.reordered: list[list[str]]
         self.checksum: int
-
         if value_type == ValueType.SHORT:
             self.input = list("12345")
             self.unpacked = list("0..111....22222")
             self.reordered = list("022111222......")
-            self.checksum = 7
+            self.checksum = (0*0) + (1*2) + (2*2) + (3*1) + (4*1) + (5*1) + (6*2) + (7*2) + (8*2)
+        elif value_type == ValueType.MEDIUM:
+            self.input = list("2333133121414131402")
+            self.unpacked = list("00...111...2...333.44.5555.6666.777.888899")
+            self.reordered = list("0099811188827773336446555566..............")
+            self.checksum = 1928
         elif value_type == ValueType.LONG:
             self.input = list("011111111111111111111111")
-            self.unpacked = ["0", ".", "1", ".", "2", ".", "3", ".", "4", ".", "5", ".", "6", ".", "7", ".", "8", ".", "9", ".", "10", ".", "11", "."]
-            self.reordered = ["0", "11", "1", "10", "2", "9", "3", "8", "4", "7", "5", "6", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
-            self.checksum = 0+10+18+24+28+30
+            self.unpacked = list(".1.2.3.4.5.6.7.8.9.") + ["10", ".", "11", "."]
+            self.reordered = ["11", "1", "10"] + list("29384756............")
+            self.checksum = (0*11) + (1*1) + (2*10) + (3*2) + (4*9) + (5*3) + (6*8) + (7*4) + (8*7) + (9*5) + (10*6)
         elif value_type == ValueType.UNEVEN:
-            self.input = list("013")
-            self.unpacked = ["0", ".", "1", "1", "1"]
-            self.reordered = ["0", "1", "1", "1", "."]
-            self.checksum = 0+1+0
+            self.input = list("113")
+            self.unpacked = list("0.111")
+            self.reordered = list("0111.")
+            self.checksum = (0*0) + (1*1) + (2*1) + (3*1)
 
 
 @pytest.fixture(scope="module", params=ValueType)
