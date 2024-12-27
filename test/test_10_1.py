096d1d38670b99af8c46da4982e74b3edc563400
fynn <fynn-git.wi04u@slmail.me>
2024-12-27 22:31:09 +0100

    more test cases

--- test/test_10_1.py
+++ test/test_10_1.py
@@ -7,49 +7,83 @@ import aoc
 import aoc_10_1
 
 
-
 class TestCase(Enum):
     A = "A"
     B = "B"
     C = "C"
+    D = "D"
+    E = "E"
 
 
 class Values:
     def __init__(self, case_type: TestCase):
         self.case_type = case_type
         self.input: aoc_10_1.MAP = []
-        self.trail_score: int = None
+        self.score: int = 0
 
         if case_type is TestCase.A:
-            self.input = "0123\n1234\n8765\n9876\n"
-            self.map: aoc_10_1.MAP = [
-                [0,1,2,3],
-                [1,2,3,4],
-                [8,7,6,5],
-                [9,8,7,6],
+            self.input = [
+                "0123",
+                "1234",
+                "8765",
+                "9876",
             ]
-            self.tails = [
-                [
-                    (0,0),
-                    (1,0),
-                    (2,0),
-                    (3,0),
-                    (3,1),
-                    (3,2),
-                    (3,3),
-                    (2,3),
-                    (1,3),
-                    (0,3),
-                ]
+            self.score = 1
+        elif case_type is TestCase.B:
+            self.input = [
+                "...0...",
+                "...1...",
+                "...2...",
+                "6543456",
+                "7.....7",
+                "8.....8",
+                "9.....9",
             ]
-            self.trail_scores = [
-                1,
+            self.score = 2
+        elif case_type is TestCase.C:
+            self.input = [
+                "..90..9",
+                "...1.98",
+                "...2..7",
+                "6543456",
+                "765.987",
+                "876....",
+                "987....",
             ]
+            self.score = 4
+        elif case_type is TestCase.D:
+            self.input = [
+                "10..9..",
+                "2...8..",
+                "3...7..",
+                "4567654",
+                "...8..3",
+                "...9..2",
+                ".....01",
+            ]
+            self.score = 3
+        elif case_type is TestCase.E:
+            self.input = [
+                "89010123",
+                "78121874",
+                "87430965",
+                "96549874",
+                "45678903",
+                "32019012",
+                "01329801",
+                "10456732",
+            ]
+            self.score = 36
 
 
 @pytest.fixture(params=TestCase)
 def values(request):
     return Values(request.param)
+
+
+def test_trails(values):
+    result = aoc_10_1.
+    assert values.
 
 
 def test_main():
